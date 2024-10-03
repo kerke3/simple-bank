e4eb56ec79f34fbedd1cb60ef54f7a25bd24c56f
kerke3 <annass.1996@gmail.com>
2024-10-03 06:13:52 +0400

    - Created two simple unit tests for illustration purposes

--- src/test/java/com/kerke3/simple_bank/SimpleBankApplicationTests.java
+++ src/test/java/com/kerke3/simple_bank/SimpleBankApplicationTests.java
@@ -1,13 +1,60 @@
 package com.kerke3.simple_bank;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kerke3.simple_bank.dto.StandardResponse;
+import com.kerke3.simple_bank.dto.UserIdRequest;
+import com.kerke3.simple_bank.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.HashMap;
+import java.util.List;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class SimpleBankApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+
 	@Test
-	void contextLoads() {
+	@Order(1)
+	void ShouldFailUserCreate() throws Exception {
+		HashMap<String,String> badRequestBody = new HashMap<>();
+		badRequestBody.put("user","anas");
+		String userRequestJSON = objectMapper.writeValueAsString(badRequestBody);
+		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/account/create").contentType(MediaType.APPLICATION_JSON).content(userRequestJSON))
+				.andExpect(MockMvcResultMatchers.status().isBadRequest()).andReturn();
+		StandardResponse response = objectMapper.readValue(result.getResponse().getContentAsString(),new TypeReference<StandardResponse>(){});
+		Assertions.assertFalse(response.success());
+	}
+	@Test
+	@Order(2)
+	void ShouldCreateUser() throws Exception {
+		UserIdRequest userIdRequest = getUserIdRequest();
+		String userRequestJSON = objectMapper.writeValueAsString(userIdRequest);
+		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/account/create").contentType(MediaType.APPLICATION_JSON).content(userRequestJSON))
+				.andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+		StandardResponse response = objectMapper.readValue(result.getResponse().getContentAsString(),new TypeReference<StandardResponse>(){});
+		Assertions.assertTrue(response.success());
+	}
+
+	private UserIdRequest getUserIdRequest() {
+		return new UserIdRequest("anas");
 	}
 
 }
