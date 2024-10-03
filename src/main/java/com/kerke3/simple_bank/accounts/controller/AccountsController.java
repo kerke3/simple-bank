7d7e3b841be50385f8be45d5ea2d593af73af1f2
kerke3 <annass.1996@gmail.com>
2024-10-03 14:25:06 +0400

    - rename tests - organize DTOs

--- src/main/java/com/kerke3/simple_bank/accounts/controller/AccountsController.java
+++ src/main/java/com/kerke3/simple_bank/accounts/controller/AccountsController.java
@@ -1,6 +1,7 @@
 package com.kerke3.simple_bank.accounts.controller;
 
 
+import com.kerke3.simple_bank.accounts.dto.request.UserAccountRequest;
 import com.kerke3.simple_bank.accounts.dto.response.UserAccountsResponse;
 import com.kerke3.simple_bank.accounts.service.AccountService;
 import com.kerke3.simple_bank.common.dto.request.UserIdRequest;
--- src/main/java/com/kerke3/simple_bank/accounts/service/AccountService.java
+++ src/main/java/com/kerke3/simple_bank/accounts/service/AccountService.java
@@ -1,5 +1,6 @@
 package com.kerke3.simple_bank.accounts.service;
 
+import com.kerke3.simple_bank.accounts.dto.request.UserAccountRequest;
 import com.kerke3.simple_bank.accounts.dto.response.UserAccountsResponse;
 import com.kerke3.simple_bank.common.dto.request.UserIdRequest;
 
